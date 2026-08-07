885aeb1f3d8e25ab8dd4071dc9b3bd6e3369ccc6
Jagadesh R <jagadeshrvs@gmail.com>
2026-08-07 12:34:56 +0530

    Change in the condition

--- GreatestAmongThree.java
+++ GreatestAmongThree.java
@@ -10,9 +10,9 @@ public class GreatestAmongThree {
         System.out.print("Enter third number: ");
         int num3 = sc.nextInt();
 
-        if (num1 >= num2 && num1 >= num3) {
+        if (num1 > num2 && num1 > num3) {
             System.out.println(num1 + " is the greatest number.");
-        } else if (num2 >= num1 && num2 >= num3) {
+        } else if (num2 > num1 && num2 > num3) {
             System.out.println(num2 + " is the greatest number.");
         } else {
             System.out.println(num3 + " is the greatest number.");
