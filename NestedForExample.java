dc20d50b206360a04accf47b1fd476c4c47a44bc
Jagadesh R <jagadeshrvs@gmail.com>
2026-08-08 11:14:33 +0530

    Changes in the print of nested loop problem

--- NestedForExample.java
+++ NestedForExample.java
@@ -3,9 +3,9 @@ public class NestedForExample{
         int n=5;
         for(int i = 1; i <= n; i++) {
             for(int j = 1; j <= n; j++) {
-                System.out.print(i + "" + j);
+                System.out.print("i: " + i + ", j: " + j);
             }
-            System.out.println();
+          
         }
     }
 }
