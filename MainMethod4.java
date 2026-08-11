e412bd0d137145ad829a906a5717642430697e6f
Jagadesh R <jagadeshrvs@gmail.com>
2026-08-11 10:30:49 +0530

    Add MainMethod4 program to demonstrate addition functionality

---
+++ MainMethod4.java
@@ -0,0 +1,14 @@
+public class MainMethod4 {
+    public void addValue() {
+        int a = 10;
+        int b = 12;
+        int sum = a + b;
+        System.out.println("Res: " + sum);
+    }
+
+    public static void main(String[] args) {
+        MainMethod4 obj = new MainMethod4();
+        obj.addValue();
+
+    }
+}
