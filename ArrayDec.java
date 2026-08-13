3fcfe109beacd3031850da3f43acda92cfed3dc4
Jagadesh R <jagadeshrvs@gmail.com>
2026-08-13 10:23:53 +0530

    Add ArrayDec program to demonstrate array declaration and iteration

---
+++ ArrayDec.java
@@ -0,0 +1,10 @@
+public class ArrayDec {
+    public static void main(String[] args) {
+        int marks[] = { 98, 88, 56, 75 };
+        int len = marks.length;
+
+        for (int i = 0; i < len; i++) {
+            System.out.println(marks[i]);
+        }
+    }
+}
