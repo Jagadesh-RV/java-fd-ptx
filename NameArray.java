87f46d24cf8db3f62b81d408e05fb218d414c184
Jagadesh R <jagadeshrvs@gmail.com>
2026-08-13 10:44:03 +0530

    Update printArray method to use print instead of println for array elements

--- NameArray.java
+++ NameArray.java
@@ -4,7 +4,7 @@ public class NameArray {
     public static void printArray(String[] arr) {
         System.out.println("The array elements are :");
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+            System.out.print(arr[i]+" ");
         }
     }
 
