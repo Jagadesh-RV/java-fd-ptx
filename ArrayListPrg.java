b1269aaee8f7316bc5a1a1ee0b76ce11d968ee83
Jagadesh R <jagadeshrvs@gmail.com>
2026-08-17 09:57:11 +0530

    Add program to define and work on the ArrayList with adding the string into it

---
+++ ArrayListPrg.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class ArrayListPrg{
+    public static void main(String[] args){
+        //Declare ArrayLIst-p String
+        ArrayList<String> list=new ArrayList<>();
+        //add() -> to insert element ed of the ArrayList 
+        list.add("is");
+        list.add("a");
+        //addFirst() -> to add the element front of ArrayList
+        
+        //to print the ArrayList
+        System.out.println("List : " +list);
+    }
+
+}
