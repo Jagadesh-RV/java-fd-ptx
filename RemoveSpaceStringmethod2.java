9cd4b2e487c3c59990df4f4df64c6d5f18024d9b
Jagadesh R <jagadeshrvs@gmail.com>
2026-08-18 11:22:24 +0530

    Add a program to remove white spaces in the string using replace function

---
+++ RemoveSpaceStringmethod2.java
@@ -0,0 +1,7 @@
+public class RemoveSpaceStringmethod2 {
+    public static void main(String [] args){
+        String str= "Java Programming Language";
+        String newstr=str.replace(" ", "");
+        System.out.println("Result :"+ newstr);
+    }
+}
