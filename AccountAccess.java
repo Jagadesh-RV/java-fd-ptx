64c6d87f3691c00868fc9c8bba2ae8ab44633bb6
Jagadesh R <jagadeshrvs@gmail.com>
2026-09-22 10:42:52 +0530

    Refactor Account class to improve encapsulation and add balance management methods

--- AccountAccess.java
+++ AccountAccess.java
@@ -1,12 +1,29 @@
-class Account{
+//encapsulation
+class Account {
     private long account_no;
     private double balance;
     private String name;
-    public Account(long account_no, double balance , String name ){
-        this.account_no=account_no;
-        this.balance=balance;
-        this.name=name;
+
+    public void setB(double B) {
+        if (B < 0) {
+            System.out.println("B is invalid");
+            return;
+        } else {
+            this.balance = B;
+        }
+
     }
+
+    public double getB() {
+        return balance;
+    }
+
+    public Account(long account_no, double balance, String name) {
+        this.account_no = account_no;
+        this.balance = balance;
+        this.name = name;
+    }
+
     public void Access() {
         System.out.println("Account No:" + account_no);
         System.out.println("Name :" + name);
@@ -14,9 +31,11 @@ class Account{
     }
 }
 
-public class AccountAccess{
-    public static void main(String [] args){
-        Account a=new Account(1010101,1000,"Jagadesh");
-        a.Access();
+public class AccountAccess {
+    public static void main(String[] args) {
+        Account a = new Account(1010101, 1000, "Jagadesh");
+        System.out.println(a.getB());
+        a.setB(10000.00);
+        System.out.println(a.getB());
     }
 }
